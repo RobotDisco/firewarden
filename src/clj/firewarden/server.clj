e6763b695459fe487aeb4d4f72e4bf2e7138a8e8
Gaelan D'costa <gdcosta@gmail.com>
2019-08-22 10:33:41 -0400

    JSONify employee list in handler.
    Also some refactoring for general ring response processing.

--- src/clj/firewarden/server.clj
+++ src/clj/firewarden/server.clj
@@ -4,16 +4,22 @@
 
 (defonce employee-list (atom {}))
 
+(defn json-response [status content]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (-> content
+             ->json)})
+
+(def successful-json-response
+  (partial json-response 200))
+
 (defn fetch-employees! []
   (when-not (seq @employee-list)
     (reset! employee-list (emp/local-employees "Toronto")))
-  (-> @employee-list
-      ->json))
+  @employee-list)
 
 (defn reset-employeelist! []
   (reset! employee-list {}))
 
 (defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (fetch-employees!)})
+  (successful-json-response (fetch-employees!)))
