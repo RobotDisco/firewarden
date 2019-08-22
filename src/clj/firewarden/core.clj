9cc5d3fb41bf9dfd8dad1ac477f95d5efeb7459d
Gaelan D'costa <gdcosta@gmail.com>
2019-08-21 22:23:30 -0400

    Readability, limit who's out list to today, code standards.
    * Add .editorconfig to enforce certain code style rules
    * Document functions and vars
    * Adhere to bbatsov's Clojure Style Guide
    * Limit "Who's out" request to fetch people from today.
    * Add JSON libraries, we'll need them soon
    * Don't extract body from HTTP requests immediately, let's process our
    responses in steps.

--- src/clj/firewarden/core.clj
+++ src/clj/firewarden/core.clj
@@ -1,13 +1,42 @@
 (ns firewarden.core
-  (:require [clj-http.client :as http]
+  (:require [cheshire.core :as json]
+            [clj-http.client :as http]
+            [java-time :as dt]
             [environ.core :refer [env]]))
 
-(def directory-url "https://api.bamboohr.com/api/gateway.php/tulip/v1/employees/directory/")
-(def out-of-office-url "https://api.bamboohr.com/api/gateway.php/tulip/v1/time_off/whos_out/")
-(def token (env :bamboohr-api-key))
+(def directory-url
+  "https://api.bamboohr.com/api/gateway.php/tulip/v1/employees/directory/")
+(def out-of-office-url
+  "https://api.bamboohr.com/api/gateway.php/tulip/v1/time_off/whos_out/")
 
-(defn fetch-employee-list []
-  (http/get directory-url {:accept :json :basic-auth [token "x"]}))
+(def bamboo-hr-api-key
+  (env :bamboohr-api-key))
+(def basic-auth-credentials
+  "BambooHR uses the API Key as the username, the password can be anything."
+  [bamboo-hr-api-key "x"])
 
-(defn fetch-out-of-office []
-  (http/get out-of-office-url {:accept :json :basic-auth [token "x"]}))
+(def base-http-request-options
+  "Basic options to pass `clj-http` when making HTTP requests
+  We will always want responses back in JSON fpormat, and we will always have to
+  supply the API token as part of BambooHR's basic auth requirements."
+  {:accept :json
+   :basic-auth basic-auth-credentials})
+
+(defn today-str [] (str (dt/local-date)))
+
+(defn fetch-employee-list
+  "Call Employee List and return `clj-http` response object."
+  []
+  (http/get directory-url
+            base-http-request-options))
+
+(defn fetch-out-of-office
+  "Call Out of Office List for today and return `clj-http` response object.
+  We always want the list of people for today and today only."
+  []
+  (let [today (today-str)]
+    (http/get
+     out-of-office-url
+     (merge base-http-request-options
+            {:query-params {"start" today
+                            "end" today}}))))
