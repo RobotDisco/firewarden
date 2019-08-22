69ed3c1f1fd2a2afd568bf7444cba9478c9def04
Gaelan D'costa <gdcosta@gmail.com>
2019-08-21 20:52:23 -0400

    Basic fetches of BambooHR data.
    Use environment variable to pass in API token.

--- src/clj/firewarden/core.clj
+++ src/clj/firewarden/core.clj
@@ -1,1 +1,13 @@
-(ns firewarden.core)
+(ns firewarden.core
+  (:require [clj-http.client :as http]
+            [environ.core :refer [env]]))
+
+(def directory-url "https://api.bamboohr.com/api/gateway.php/tulip/v1/employees/directory/")
+(def out-of-office-url "https://api.bamboohr.com/api/gateway.php/tulip/v1/time_off/whos_out/")
+(def token (env :bamboohr-api-key))
+
+(defn fetch-employee-list []
+  (http/get directory-url {:accept :json :basic-auth [token "x"]}))
+
+(defn fetch-out-of-office []
+  (http/get out-of-office-url {:accept :json :basic-auth [token "x"]}))
