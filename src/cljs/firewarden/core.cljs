fddde7d263ff24d3ba0d25b361eb7c2b9dd4eb6a
Gaelan D'costa <gdcosta@gmail.com>
2019-08-22 13:02:32 -0400

    Grabbing data from backend, displaying in text reagent widget

--- src/cljs/firewarden/core.cljs
+++ src/cljs/firewarden/core.cljs
@@ -1,13 +1,22 @@
 (ns ^:figwheel-hooks firewarden.core
+  (:require-macros
+   [cljs.core.async.macros :refer [go]])
   (:require
+   [cljs.core.async :refer [<!]]
+   [cljs-http.client :as http]
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]))
 
 ;; application state
-(defonce app-state (atom {:__figwheel_counter 0, :in-office [], :out-of-office []}))
+(defonce app-state (atom {:__figwheel_counter 0, :employees []}))
+
+(defn fetch-employee-list! []
+  (go (let [response (<! (http/get "http://localhost:9500/whatever"))]
+        (swap! app-state assoc :employees (:body response)))))
 
 (defn reset-app-state! []
-  (reset! app-state {:__figwheel_counter 0, :in-office [], :out-of-office []}))
+  (swap! app-state assoc :__figwheel_counter 0)
+  fetch-employee-list!)
 
 (defn get-app-element []
   (gdom/getElement "app"))
@@ -26,10 +35,12 @@
 ;; conditionally start your applucation based on the presence of an "app" element
 ;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
+(when-not (seq (:employees @app-state))
+  (fetch-employee-list!))
 
 ;; specify reload-hook with ^:after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  (swap! app-state update-in [:__figwheel_counter] inc))
+  (swap! app-state update :__figwheel_counter inc))
