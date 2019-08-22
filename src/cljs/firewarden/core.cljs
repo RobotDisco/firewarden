2aae1eac76eeb21a9396512c8947dc86ae6df501
Gaelan D'costa <gdcosta@gmail.com>
2019-08-22 11:34:57 -0400

    Simple reagent widget, reflect browser-side app state.

--- src/cljs/firewarden/core.cljs
+++ src/cljs/firewarden/core.cljs
@@ -1,1 +1,35 @@
-(ns firewarden.core)
+(ns ^:figwheel-hooks firewarden.core
+  (:require
+   [goog.dom :as gdom]
+   [reagent.core :as reagent :refer [atom]]))
+
+;; application state
+(defonce app-state (atom {:__figwheel_counter 0, :in-office [], :out-of-office []}))
+
+(defn reset-app-state! []
+  (reset! app-state {:__figwheel_counter 0, :in-office [], :out-of-office []}))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(defn hello-world []
+  [:div
+   [:h4 (str @app-state)]])
+
+(defn mount [elem]
+  (reagent/render-component [hello-world] elem))
+
+(defn mount-app-element []
+  (when-let [elem (get-app-element)]
+    (mount elem)))
+
+;; conditionally start your applucation based on the presence of an "app" element
+;; this is particularly helpful for testing this ns without launching the app
+(mount-app-element)
+
+;; specify reload-hook with ^:after-load metadata
+(defn ^:after-load on-reload []
+  (mount-app-element)
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  (swap! app-state update-in [:__figwheel_counter] inc))
