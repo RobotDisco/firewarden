27aa075ff4c1e1026f87c4b64272fb243a89ac78
Gaelan D'costa <gdcosta@gmail.com>
2019-08-25 13:15:37 -0400

    Refactoring: Extracting app-state functionality to its own module.

--- src/cljs/firewarden/core.cljs
+++ src/cljs/firewarden/core.cljs
@@ -1,31 +1,8 @@
 (ns ^:figwheel-hooks firewarden.core
-  (:require-macros
-   [cljs.core.async.macros :refer [go]])
   (:require
-   [cljs.core.async :refer [<!]]
-   [cljs-http.client :as http]
+   [firewarden.state :as fws]
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]))
-
-;; application state
-(defonce app-state (atom {:__figwheel_counter 0, :employees []}))
-
-(defn fetch-employee-list! []
-  (go (let [response (<! (http/get "http://localhost:9500/whatever"))
-            employees (:body response)
-            employees-accounted (map #(assoc % :accounted-for? false) employees)
-            emp-map (zipmap (map :id employees-accounted) employees-accounted)]
-        (swap! app-state assoc :employees emp-map))))
-
-(defn reset-app-state! []
-  (swap! app-state assoc :__figwheel_counter 0)
-  fetch-employee-list!)
-
-;; Events
-(defn account-for [emp-id]
-  (fn [e]
-    (prn emp-id)
-    (swap! app-state update-in [:employees emp-id :accounted-for?] not)))
+   [reagent.core :as reagent]))
 
 ;; Widgets -- move somewhere else
 (defn employee-info
@@ -34,7 +11,7 @@
          accounted-for? :tr.accounted-for
          :else :tr)
    (when in-office?
-     {:on-mouse-down (account-for id)})
+     {:on-mouse-down (fws/account-for id)})
    [:td [(if in-office? :img.profile :img.profile.grayscale) {:src photoUrl}]]
    [:td firstName " " lastName]])
 
@@ -46,7 +23,7 @@
 
 (defn firewarden-app []
   [:div
-   [employee-list (sort-by :in-office? (sort-by :lastName (vals (:employees @app-state))))]])
+   [employee-list (sort-by :in-office? (sort-by :lastName (vals (:employees @fws/app-state))))]])
 
 ;; Reagent bootstrapping
 
@@ -63,12 +40,9 @@
 ;; conditionally start your application based on the presence of an "app" element
 ;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
-(when-not (seq (:employees @app-state))
-  (fetch-employee-list!))
+(when-not (seq (:employees @fws/app-state))
+  (fws/fetch-employee-list!))
 
 ;; specify reload-hook with ^:after-load metadata
 (defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  (swap! app-state update :__figwheel_counter inc))
+  (mount-app-element))
---
+++ src/cljs/firewarden/state.cljs
@@ -0,0 +1,33 @@
+(ns ^:figwheel-hooks firewarden.state
+  (:require-macros
+   [cljs.core.async.macros :refer [go]])
+  (:require
+   [cljs.core.async :refer [<!]]
+   [cljs-http.client :as http]
+   [reagent.core :refer [atom]]))
+
+;; application state
+(defonce app-state (atom {:__figwheel_counter 0, :employees []}))
+
+;; Actions/Events ... things that change app date
+(defn fetch-employee-list! []
+  (go (let [response (<! (http/get "http://localhost:9500/whatever"))
+            employees (:body response)
+            employees-accounted (map #(assoc % :accounted-for? false) employees)
+            emp-map (zipmap (map :id employees-accounted) employees-accounted)]
+        (swap! app-state assoc :employees emp-map))))
+
+(defn account-for [emp-id]
+  (fn [e]
+    (prn emp-id)
+    (swap! app-state update-in [:employees emp-id :accounted-for?] not)))
+
+;; Actions designed for working with state when developing
+(defn reset-app-state! []
+  (swap! app-state assoc :__figwheel_counter 0)
+  fetch-employee-list!)
+
+(defn ^:after-load on-reload []
+  (swap! app-state update :__figwheel_counter inc))
+
+
